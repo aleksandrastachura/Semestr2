271971bfd7e3fdd77690f60732f2ea0c737a5ab5
Aleksandra Stachura <aleksandra.stachura@shoper.pl>
2022-09-21 14:33:20 +0200

    zadanie_1

--- Shopstagram/js/app.js
+++
@@ -1,6 +0,0 @@
-const postLoaderButton = document.querySelector(".post--loader");
-const hiddenPost = document.querySelector(".post.hidden");
-
-postLoaderButton.addEventListener("click", function() {
-    hiddenPost.classList.remove("hidden");
-});
